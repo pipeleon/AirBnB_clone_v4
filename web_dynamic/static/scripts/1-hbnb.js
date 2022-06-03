367503d489f1a9d1bf95a0b715288162483ef4fa
pipeleon <oflg94@gmail.com>
2022-06-03 17:11:50 -0500

    fixing .js file for check amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,52 @@
-window.onload = () => {
-    const input_checkbox_list = [...document.querySelectorAll('input[type=checkbox]')];
-    const h4_amenities = document.querySelector('h4');
-    let amenities_list = [];
-    for (let input_checkbox of input_checkbox_list) {
-        input_checkbox.addEventListener('change', () => {
-            if (this.checked) {
-                amenities_list.push(`${input_checkbox.data-id}`);
-            } else {
-                for (let i = 0; i < amenities_list.length; i++) {
-                    if (amenities_list[i] == `${input_checkbox.data-id}`) {
-                        amenities_list.splice(i, 1);
-                        i--;
-                    }
-                }
-            }
-        })
-    }
-    console.log(amenities_list);
-}
+// window.onload = () => {
+//     const input_checkbox_list = [...document.querySelectorAll('input[type=checkbox]')];
+//     const h4_amenities = document.querySelector('h4');
+//     let amenities_list = [];
+//     for (let input_checkbox of input_checkbox_list) {
+//         input_checkbox.addEventListener('change', () => {
+//             if (this.checked) {
+//                 amenities_list.push(`${input_checkbox.data-id}`);
+//             } else {
+//                 for (let i = 0; i < amenities_list.length; i++) {
+//                     if (amenities_list[i] == `${input_checkbox.data-id}`) {
+//                         amenities_list.splice(i, 1);
+//                         i--;
+//                     }
+//                 }
+//             }
+//         })
+//     }
+//     console.log(amenities_list);
+// }
+
+const input_checkbox_list = [...document.querySelectorAll('input[type=checkbox]')];
+const h4_amenities = document.querySelector('h4');
+
+check_amenities = []
+check_names = []
+input_checkbox_list.forEach(($check) => {
+    $check.addEventListener('change', () => {
+        if ($check.checked) {
+            check_amenities.push($check.getAttribute('data-id'))
+            check_names.push($check.getAttribute('data-name'))
+            text_names = ""
+            check_names.forEach((check_name) => {
+                text_names = text_names + check_name
+            })
+            h4_amenities.text = text_names
+            console.log(check_amenities)
+        }
+        else {
+            const id = check_amenities.indexOf($check.getAttribute('data-id'))
+            check_amenities.splice(id, 1)
+            const id2 = check_names.indexOf($check.getAttribute('data-name'))
+            check_names.splice(id2, 1)
+            text_names = ""
+            check_names.forEach((check_name) => {
+                text_names = text_names + check_name
+            })
+            h4_amenities.text = text_names
+            console.log(check_amenities)
+        }
+    })
+})
