5864f29cb30a0c93cf611aeb421ba30b851f5120
pipeleon <oflg94@gmail.com>
2022-06-04 16:00:40 -0500

    addding funcion for render placees acoording to API query

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,6 +1,7 @@
 const input_checkbox_list = [...document.querySelectorAll('input[type=checkbox]')];
 const h4_amenities = [...document.querySelectorAll('h4')];
 const circle = document.querySelector("div#api_status")
+const places_seccion = document.querySelector(".places")
 
 check_amenities = []
 check_names = []
@@ -64,8 +65,55 @@ async function get_places () {
         },
         body: JSON.stringify(info)
     });
-    const flag = await response.json();
-    console.log(flag.length)
+    const places = await response.json();
+    places.forEach((place) => {
+        const art = document.createElement('article')
+        const title = document.createElement('div')
+        title.className = "title_box"
+        const title_h2 = document.createElement('h2')
+        title_h2.textContent = place.name
+        const price = document.createElement('div')
+        price.className = "price_by_night"
+        price.textContent = `$${place.price_by_night}`
+        title.appendChild(title_h2)
+        title.appendChild(price)
+        art.appendChild(title)
+        const information = document.createElement('div')
+        information.className = "information"
+        const guests = document.createElement('div')
+        guests.className = "max_guest"
+        if (place.max_guest > 1) {
+            guests.textContent = `${place.max_guest} Guests`
+        }
+        else {
+            guests.textContent = `${place.max_guest} Guest`
+        }
+        const rooms = document.createElement('div')
+        rooms.className = "number_rooms"
+        if (place.number_rooms > 1) {
+            rooms.textContent = `${place.number_rooms} Rooms`
+        }
+        else {
+            rooms.textContent = `${place.number_rooms} Room`
+        }
+        const baths = document.createElement('div')
+        baths.className = "number_bathrooms"
+        if (place.number_bathrooms > 1) {
+            baths.textContent = `${place.number_bathrooms} Bathrooms`
+        }
+        else {
+            baths.textContent = `${place.number_bathrooms} Bathroom`
+        }
+        information.appendChild(guests)
+        information.appendChild(rooms)
+        information.appendChild(baths)
+        art.appendChild(information)
+        const description = document.createElement('div')
+        description.className = "description"
+        description.innerHTML = place.description
+        art.appendChild(description)        
+        places_seccion.appendChild(art)
+    })
 }
 
 get_places()
@@ -73,7 +121,6 @@ get_places()
 async function api_status () {
     const response = await fetch('http://localhost:5001/api/v1/status/');
     const flag = await response.json();
-    console.log(flag)
     if (flag["status"] == "OK") {
         circle.classList.add("available")
     }
